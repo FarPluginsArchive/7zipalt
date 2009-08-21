b99ed172d0bc669038bd6c0ab786a65d9c810ac4
farplug <farplug@ef865a02-7c4e-11de-a32a-85815177f542>
2009-08-21 19:45:15 +0000

    делаем очистку перед сборкой дистрибутивов

--- distrib.bat
+++ distrib.bat
@@ -2,6 +2,8 @@ set build=20
 rem SRC\Common\FILE_ID.DIZ
 rem SRC\CPP\7zip\MyVersion.h
 
+call :clean
+
 vcbuild /nologo SRC\CPP\7zip\UI\Far\Far_VC9.vcproj "Release|Win32" 
 @if errorlevel 1 goto error
 vcbuild /nologo SRC\CPP\7zip\UI\Far\Far_VC9.vcproj "Release x64|x64"
@@ -16,11 +18,15 @@ call :package PluginW64 b%build%\7zip-465alt64-20-b%build%.zip
 call :package PluginA b%build%\7zip-465alt-b%build%.zip
 call :package PluginA64 b%build%\7zip-465alt64-b%build%.zip
 
+call :clean
+
+@goto end
+
+:clean
 vcbuild /clean /nologo SRC\CPP\7zip\UI\Far\Far_VC9.vcproj "Release|Win32" 
 vcbuild /clean /nologo SRC\CPP\7zip\UI\Far\Far_VC9.vcproj "Release x64|x64"
 vcbuild /clean /nologo SRC\CPP\7zip\UI\Far\Far_VC9.vcproj "ReleaseA|Win32" 
 vcbuild /clean /nologo SRC\CPP\7zip\UI\Far\Far_VC9.vcproj "ReleaseA x64|x64"
-
 @goto end
 
 :package
